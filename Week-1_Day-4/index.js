d8cbde17ffca55c6b7d983254fff9985ba7b3939
Tamunoenefaa Harry <tamunoenefaa@gmail.com>
2018-05-29 19:28:22 -0400

    Clone the template qhile adding a flick

--- Week-1_Day-4/index.js
+++ Week-1_Day-4/index.js
@@ -1,37 +1,47 @@
 const app = {
-    init: function(selectors) {
-      this.max = 0
-      this.list = document.querySelector(selectors.listSelector)
-  
-      document
-        .querySelector(selectors.formSelector)
-        .addEventListener('submit', ev => {
-          ev.preventDefault()
-          this.handleSubmit(ev)
-        })
-    },
-  
-    renderListItem: function(flick) {
-      const item = document.createElement('li')
-      item.textContent = flick.name
-      return item
-    },
-  
-    handleSubmit: function(ev) {
-      const f = ev.target
-      const flick = {
-        id: ++this.max,
-        name: f.flickName.value,
-      }
-  
-      const item = this.renderListItem(flick)
-      this.list.appendChild(item)
-  
-      f.reset()
-    },
-  }
-  
-  app.init({
-    formSelector: '#flickForm',
-    listSelector: '#flickList',
-  })
+  init(selectors) {
+    this.flicks = []
+    this.max = 0
+    this.list = document.querySelector(selectors.listSelector)
+    this.template = document.querySelector(selectors.templateSelector)
+
+    document
+      .querySelector(selectors.formSelector)
+      .addEventListener('submit', ev => {
+        ev.preventDefault()
+        this.handleSubmit(ev)
+      })
+  },
+
+  renderListItem(flick) {
+    const item = this.template.cloneNode(true)
+    item.classList.remove('template')
+    item.dataset.id = flick.id
+    item
+      .querySelector('.flickName')
+      .textContent = flick.name
+
+    return item
+  },
+
+  handleSubmit(ev) {
+    const f = ev.target
+    const flick = {
+      id: ++this.max,
+      name: f.flickName.value,
+    }
+
+    this.flicks.unshift(flick)
+
+    const item = this.renderListItem(flick)
+    this.list.insertBefore(item, this.list.firstElementChild)
+
+    f.reset()
+  },
+}
+
+app.init({
+  formSelector: '#flickForm',
+  listSelector: '#flickList',
+  templateSelector: '.flick.template',
+})
